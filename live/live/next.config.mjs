b4740a818ac73eeeb521798a1e42835ec1626073
BLAZZY <XAPP431@GMAIL.COM>
2025-03-23 23:59:05 -0400

    Update next.config.mjs

--- live/live/next.config.mjs
+++ live/live/next.config.mjs
@@ -1,16 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["utfs.io"],
+    domains: ["utfs.io"], // Allows images from utfs.io domain
   },
   webpack: (config) => {
     config.module.rules.push({
-      test: /\.mjs$/,
-      include: /node_modules/,
-      type: "javascript/auto",
+      test: /\.mjs$/, // Matches .mjs files
+      include: /node_modules/, // Applies only to node_modules
+      type: "javascript/auto", // Processes .mjs files as ES modules
     });
 
-    return config;
+    return config; // Returns the modified Webpack config
   },
 };
 
